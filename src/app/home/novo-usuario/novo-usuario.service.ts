e3f8d4cf48757465e163e19370b5705e060f94d2
Toshio Watanabe <toshio_ws@hotmail.com>
2021-07-09 16:20:32 -0300

    Service cadastro usuario

---
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,16 @@
+import { NovoUsuario } from './novo-usuario';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  cadastraNovoUsuario(novoUsuario: NovoUsuario) {
+    return this.httpClient.post('http://localhost:3000/user/signup', novoUsuario);
+  }
+}
