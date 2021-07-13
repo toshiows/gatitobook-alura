423f8f7c8f7dc3b753282ae8b199f6f40fbd9d5c
Toshio Watanabe <toshio_ws@hotmail.com>
2021-07-13 00:57:42 -0300

    Validacao assincrona usuario existe service

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,3 +1,4 @@
+import { UsuarioExisteService } from './usuario-existe.service';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -13,13 +14,14 @@ export class NovoUsuarioComponent implements OnInit {
 
   novoUsuarioForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private novoUsuarioService: NovoUsuarioService) { }
+  constructor(private formBuilder: FormBuilder, private novoUsuarioService: NovoUsuarioService,
+    private usuarioExisteService: UsuarioExisteService) { }
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       fullName: ['', [Validators.required, Validators.minLength(4)]],
-      userName: ['', [Validators.required, minusculoValidator]],
+      userName: ['', [Validators.required, minusculoValidator], [this.usuarioExisteService.usuarioJaExiste()]],
       password: ['']
     });
   }
