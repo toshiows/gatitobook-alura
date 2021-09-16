4c8356536937e863bd21146c4bdc7af93bea0ad0
Toshio Watanabe <toshio_ws@hotmail.com>
2021-09-15 22:27:40 -0300

    Lista animais

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -3,18 +3,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+const API =  environment.apiURL;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutenticacaoService {
 
-  API: string = 'http://localhost:3000/user/login';
-
   constructor(private httpClient: HttpClient, private usuarioService: UsuarioService) { }
 
   autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
-    return this.httpClient.post(this.API, {
+    return this.httpClient.post(`${API}/user/login`, {
       userName: usuario,
       password: senha
     }, 
--- src/app/home/novo-usuario/novo-usuario.service.ts
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -1,6 +1,9 @@
+import { environment } from './../../../environments/environment';
 import { NovoUsuario } from './novo-usuario';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const API = environment.apiURL;
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +13,10 @@ export class NovoUsuarioService {
   constructor(private httpClient: HttpClient) { }
 
   cadastraNovoUsuario(novoUsuario: NovoUsuario) {
-    return this.httpClient.post('http://localhost:3000/user/signup', novoUsuario);
+    return this.httpClient.post(`${API}/user/signup`, novoUsuario);
   }
 
   verificaUsuarioExistente(nomeUsuario: string) {
-    return this.httpClient.get(`http://localhost:3000/user/exists/${nomeUsuario}`);
+    return this.httpClient.get(`${API}/user/exists/${nomeUsuario}`);
   }
 }
